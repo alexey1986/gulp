1e146756965f011d41568f563bebcb47810accb6
aliaksei.paleshchuk <aliaksei_paleshchuk@epam.com>
2016-12-09 14:07:39 +0300

    final changes 2

---
+++ README.md
@@ -0,0 +1,11 @@
+#To run gulp you should to do next actions:
+
+npm install gulp
+npm install --save-dev gulp-less
+npm install --save-dev gulp-concat
+npm install --save-dev gulp-cssmin
+npm install --save-dev gulp-uglify
+npm install --save-dev gulp-watch
+npm install browser-sync gulp --save-dev
+
+and run "gulp build" task
--- gulpfile.js
+++ gulpfile.js
@@ -5,31 +5,35 @@ const gulp = require('gulp'),
 	concat = require('gulp-concat'),
 	cssmin = require('gulp-cssmin'),
 	uglify = require('gulp-uglify'),
-	watch = require('gulp-watch');
+	watch = require('gulp-watch'),
+	browserSync = require('browser-sync').create();
 
-gulp.task('css', function() {
+gulp.task('styles', function() {
 	return gulp.src('./source/less/**/*.less')
 			.pipe(less())
-			.pipe(concat('all.css'))
+			.pipe(concat('all.min.css'))
 			.pipe(cssmin())
 			.pipe(gulp.dest('./destination/css'));
 });
 
-gulp.task('js', function () {
+gulp.task('scripts', function () {
 	return gulp.src('./source/js/**/*.js')
 			.pipe(concat('script.min.js'))
 			.pipe(uglify())
 			.pipe(gulp.dest('./destination/js'));
 });
 
-gulp.task('build', function(){
-	gulp.run('css', 'js');
+gulp.task('html', function () {
+	return gulp.src('./source/index.html')
+			.pipe(gulp.dest('./destination'));
+});
 
-	gulp.watch('./source/js/*.js', function(){
-		gulp.run('js');
-	});
+gulp.task('build', ['styles', 'scripts', 'html'], function () {
+    browserSync.init({
+        server: ['./destination']
+    });
 
-	gulp.watch('./source/less/*.less', function(){
-		gulp.run('css');
-	});
+    gulp.watch(['./source/less/*.less'], ['styles', browserSync.reload]);
+    gulp.watch(['./source/js/*.js'], ['scripts', browserSync.reload]);
+    gulp.watch(['./source/index.html'], ['html', browserSync.reload]);
 });
